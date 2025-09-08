fd67ac58c40febace229dacc75a381fbac888ab9
LuisFGom <luisgomezllerena2.0@gmail.com>
2025-09-08 09:53:59 -0500

    Creacion de Constructores

--- NorthWind.Sales.Entities/Dtos/CreateOrder/CreateOrderDetailDtos.cs
+++ NorthWind.Sales.Entities/Dtos/CreateOrder/CreateOrderDetailDtos.cs
@@ -1,11 +1,83 @@
 ﻿namespace NorthWind.Sales.Entities.Dtos.CreateOrder;
 
+// PARA COMENTAR TODO UNA SOLA SEÑALAR Y CONTROL + K + C
 
-// Primary Constructors c# 12
-public class CreateOrderDetailDtos (int ProductId, decimal UnitPrice)
+// PARA DESCOMENTAR TODO UNA SOLA SEÑALAR Y CONTROL + K + U
+
+#region Primary Constructors c# 12
+public class CreateOrderDetailDto (int productId, decimal unitPrice, short quantity)
 {
-    public int ProductId => ProductId;  //expresion blanda, utilice solo la propiedad de lectura
-    public decimal UnitPrice => UnitPrice;
-    public short Quantity => Quantity; 
+public int ProductId => productId;  //expresion blanda, utilice solo la propiedad de lectura
+    public decimal UnitPrice => unitPrice;
+    public short Quantity => quantity;
 }
 
+#endregion
+
+
+#region c# 2.0
+
+//EL DTO ES INMUTABLE POR ESO SOLO NECESITA EL GET
+
+//public class CreateOrderDetailDto
+//{
+//en los private se recomienda poner con el guion bajo y estilo camello (primera minuscula luego mayucula)
+//el guion bajo significa que es de clase
+//   private int _productId;
+//   private decimal _unitPrice; 
+//   private short _quantity;
+
+//   public CreateOrderDetailDto(int productId, decimal unitPrice, short quantity)
+//   {
+//       _productId = productId;
+//       _unitPrice = unitPrice;
+//       _quantity = quantity;
+//   }
+
+//   public int ProductId
+//   {
+//       get
+//       {
+//           return _productId;
+//       }
+//   }
+
+//public decimal UnitPrice
+//   {
+//       get
+//       {
+//           return _unitPrice;
+//       }
+//   }
+
+//public short Quantity
+//   {
+//        get
+//       {
+//          return _quantity;
+//      }
+//   }
+//}
+
+#endregion
+
+
+#region Propiedades automaticas c# 3.0
+
+//public class  CreateOrderDetailDto
+//{
+//    public int ProductId { get; private set; }  
+//    public decimal UnitPrice { get; private set; }
+//    public short Quantity { get; private set; }
+
+
+//    //PONINEDO ctor y con el tab crea automaticamente este constructor 
+//    public CreateOrderDetailDto(int productId, decimal unitPrice, short quantity)
+//    {
+//        ProductId = productId;
+//        UnitPrice = unitPrice;
+//        Quantity = quantity;
+//    }
+//}
+
+#endregion
--- NorthWind.Sales.Entities/obj/Debug/net8.0/NorthWind.Sales.Entities.AssemblyInfo.cs
+++ NorthWind.Sales.Entities/obj/Debug/net8.0/NorthWind.Sales.Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NorthWind.Sales.Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbc85fa3868c4e03f4d310f7144e754ed3c2f789")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+272294c8731b7b3c522f27c3053783a1c56b61a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("NorthWind.Sales.Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NorthWind.Sales.Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
